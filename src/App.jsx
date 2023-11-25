087d4b6183eef0cf76b444260f41b0d221dfb19f
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-11-25 09:48:11 +0100

    finished functionality for sign-up page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import Header from './components/Header/Header';
 import Home from './components/Main/Home';
 import Signup from './components/Main/Signup';
 import FarmData from './components/Main/FarmData';
+import ProtectedRoute from './components/Main/ProtectedRoute';
+import { useState } from 'react';
 
 function App() {
     const location = useLocation();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     return (
         <div className='font-inter'>
@@ -13,8 +16,18 @@ function App() {
             <main>
                 <Routes>
                     <Route path='/' element={<Home />} />
-                    <Route path='/create-account' element={<Signup />} />
-                    <Route path='/add-farm-data' element={<FarmData />} />
+                    <Route
+                        path='/create-account'
+                        element={
+                            <Signup
+                                auth={isAuthenticated}
+                                setAuth={setIsAuthenticated}
+                            />
+                        }
+                    />
+                    <Route element={<ProtectedRoute auth={isAuthenticated} />}>
+                        <Route path='/add-farm-data' element={<FarmData />} />
+                    </Route>
                 </Routes>
             </main>
         </div>
